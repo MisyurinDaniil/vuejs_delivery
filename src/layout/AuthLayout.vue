83f5160c808928451d5744e075108f1fb2da9e4b
MacBook-Air-Daniil <d.a.misyurin@gmail.com>
2025-03-23 23:07:30 +0300

    Installed from NPM Bootsrap. Added two Layout (Authlayout, Mainlayout). Remove the default components from the project. Added three main Views (Home, Help, Auth).

---
+++ src/layout/AuthLayout.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="d-flex vh-100 justify-content-center align-items-center">
+    <div class="card p-4 shadow-lg" style="width: 350px">
+      <h3 class="text-center mb-4">Вход</h3>
+      <form @submit.prevent="login">
+        <div class="mb-3">
+          <label for="email" class="form-label">Email</label>
+          <input type="email" class="form-control" id="email" required />
+        </div>
+        <div class="mb-3">
+          <label for="password" class="form-label">Пароль</label>
+          <input type="password" class="form-control" id="password" required />
+        </div>
+        <button type="submit" class="btn btn-primary w-100">Войти</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="overlay-header">
+    <h1>Home</h1>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+  .overlay-header {
+      position: fixed;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      z-index: 1050; /* Для гарантии, что элемент будет сверху */
+      background-color: rgba(0, 0, 0, 0.5); /* Полупрозрачный фон */
+      color: white;
+      padding: 20px;
+      border-radius: 5px;
+    }
+</style>
